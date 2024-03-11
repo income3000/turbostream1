b22454a95da7d7abc583a280d1a8ecafe90ae6ee
income3000 <income3000@gmail.com>
2024-03-11 10:54:44 -0400

    homepage

--- README.md
+++
@@ -1,9 +0,0 @@
-there are no dependencies or gems added yet
-need to add the following:
-rich text(action text)
-devise
-bootstrap
-
-CSS needs to be added 
-
-needs to be deployed via heroku 
--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  resources :notes
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-root "notes#index"
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
